e81a984f25ffd7d45ce11bfca49fc72b4aa6c7aa
wjc182 <782468817@qq.com>
2020-12-16 07:56:43 +0800

    works

---
+++ app/src/main/java/com/example/p7day02/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.p7day02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "复习第一天知识", Toast.LENGTH_SHORT).show();
+    }
+}
